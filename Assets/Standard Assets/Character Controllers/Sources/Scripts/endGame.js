a6b46ea52a1a5d4a2274610b06b0e36304f9c604
Jeanette Huang <jeanette.y.huang@gmail.com>
2013-06-07 14:09:55 -0500

    level complete

--- Assets/Standard Assets/Character Controllers/Sources/Scripts/endGame.js
+++ Assets/Standard Assets/Character Controllers/Sources/Scripts/endGame.js
@@ -1,25 +1,29 @@
 function OnTriggerEnter (other : Collider) {
 	// Win level 1, trigger button
 	if (other.tag == "Player" && this.name=="E8Collide-Level1") {
-		yield WaitForSeconds(0.15);
+		GameObject.Find("levelCompleteMesh").renderer.enabled = true;
+		yield WaitForSeconds(1);
 		Application.LoadLevel(2);
 	}
 
 	// Win level 2, teleport
 	if (other.tag == "Player" && this.name=="E8Collide-Level2") {
-		yield WaitForSeconds(0.15);
+		GameObject.Find("levelCompleteMesh").renderer.enabled = true;
+		yield WaitForSeconds(1);
 		Application.LoadLevel(3);
 	}
 	
 	// Win third level
 	if (other.tag == "Player" && this.name=="E8Collide-Level3") {
-		yield WaitForSeconds(0.15);
+		GameObject.Find("levelCompleteMesh").renderer.enabled = true;
+		yield WaitForSeconds(1);
 		Application.LoadLevel(4);
 	}
 	
 	// Win fourth level
 	if (other.tag == "Player" && this.name=="E8Collide-Level4") {
-		yield WaitForSeconds(0.15);
+		GameObject.Find("levelCompleteMesh").renderer.enabled = true;
+		yield WaitForSeconds(1);
 		HealthControl.LIVES = 7;
 		Application.LoadLevel(5);
 	}
