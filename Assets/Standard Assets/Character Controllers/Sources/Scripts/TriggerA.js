19a61e2f456f53ad4327b597981808cf12364936
Jeanette Huang <jeanette.y.huang@gmail.com>
2013-06-07 03:11:07 -0500

    added text

--- Assets/Standard Assets/Character Controllers/Sources/Scripts/TriggerA.js
+++ Assets/Standard Assets/Character Controllers/Sources/Scripts/TriggerA.js
@@ -45,4 +45,32 @@ function OnTriggerEnter (other : Collider){
 		}
 		
 	}
+	
+	if (other.tag == "Player" && this.name=="TriggerStar2Base" ) {
+		Debug.Log("Hit Trigger Star2");
+		Debug.Log(this.transform.parent.localScale.y);
+		if (this.transform.parent.localScale.y == 0.45) {
+			this.transform.parent.localScale.y = 0.25;
+			GameObject.Find("E7").GetComponent(moveBlock).enabled=true;
+		} else if (this.transform.parent.localScale.y == 0.25) {
+			Debug.Log("turn off");
+			this.transform.parent.localScale.y = 0.45;
+			GameObject.Find("E7").GetComponent(moveBlock).enabled=false;
+		} else {
+			this.transform.parent.localScale.y = 0.25;
+			
+			GameObject.Find("E7").GetComponent(moveBlock).enabled=true;
+		}
+		
+	}
+	
+	
+		if (other.tag == "Player" && this.name=="starCollider2") {
+			GameObject.Find("star").SetActive(false);
+			GameObject.Find("TriggerStar2").collider.enabled = true;;
+			GameObject.Find("TriggerStar2Base").collider.enabled = true;
+			GameObject.Find("TriggerStar2Base").collider.isTrigger = true;
+			GameObject.Find("TriggerStar2").renderer.material = GameObject.Find("E8").renderer.material;
+	}
 }
+
